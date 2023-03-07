26c934d5f124c821b8c27e5a4b931fcc6f09f165
Zaar <zaarinc@gmail.com>
2023-03-07 11:14:09 +0300

    1_0, 1_1, 1_2

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+//    является ли первое число квадратом второго.
+
+int num1 = 0;
+int num2 = 0;
+
+Console.Write("Write 1st number: ");
+num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Write 1st number: ");
+num2 = int.Parse(Console.ReadLine()!);
+
+if ((num2 * num2) == num1)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// 2. Напишите программу, которая будет выдавать название дня
+//    недели по заданному номеру.
+
+Console.Write("Enter number from 1 to 7: ");
+int num = int.Parse(Console.ReadLine()!);
+if(num == 1)
+{
+    Console.Write("monday");
+}
+else if(num == 2)
+{
+    Console.Write("tuesday");
+}
+else if(num == 3)
+{
+    Console.Write("wednesday");
+}
+else if(num == 4)
+{
+    Console.Write("thursday");
+}
+else if(num == 5)
+{
+    Console.Write("friday");
+}
+else if(num == 6)
+{
+    Console.Write("saturday");
+}
+else if(num == 7)
+{
+    Console.Write("sunday");
+}
+else
+{
+    Console.WriteLine("error");
+}
