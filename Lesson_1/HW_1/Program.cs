818553e0682765e4af19d84dd26c81291d4bf50a
Zaar <zaarinc@gmail.com>
2023-03-13 10:56:01 +0300

    1st lesson, HW_1

--- Lesson_1/HW_1/Program.cs
+++ Lesson_1/HW_1/Program.cs
@@ -3,3 +3,23 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.Write("Enter 1st number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter 2nd number: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter 3d number: ");
+int num3 = int.Parse(Console.ReadLine()!);
+
+if (num1 < num2)
+{
+    num1 = num2;
+}
+if (num1 < num3)
+{
+    num1 = num3;
+}
+
+Console.Write($"The number {num1} is the largest");
