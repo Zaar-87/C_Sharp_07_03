bd1d7c521d2d340b030e6e679edfb934a79fa82a
Zaar <zaarinc@gmail.com>
2023-03-13 12:28:08 +0300

    les2, task 3

--- Lesson_2/2_3/Program.cs
+++ Lesson_2/2_3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+//int number = new Random().Next(10, 100);
+int a = new Random().Next(100, 1000);
+
+void MyFunc(int num)
+{
+    Console.WriteLine(num);
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+}
+
+MyFunc(a);
