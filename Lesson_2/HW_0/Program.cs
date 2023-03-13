764d997e1f821fc29bdced5bcaf0372debb14dcb
Zaar <zaarinc@gmail.com>
2023-03-13 16:36:27 +0300

    lesson 2 -  HW_0 mod1

--- Lesson_2/HW_0/Program.cs
+++ Lesson_2/HW_0/Program.cs
@@ -4,13 +4,8 @@
 // Пример:
 // 456 -> 5 , 782 -> 8 , 918 -> 1
 
-int num = GenerateNumber();
-Console.WriteLine(num);
+Console.Write("Enter the number: ");
+int num = int.Parse(Console.ReadLine()!);
 string num_text = Convert.ToString(num);
-Console.WriteLine("2nd digit is " + num_text[1]);
 
-int GenerateNumber()
-{
-    int num = new Random().Next(100, 1000);
-    return num;
-}
+Console.WriteLine("2nd digit is " + num_text[1]);
