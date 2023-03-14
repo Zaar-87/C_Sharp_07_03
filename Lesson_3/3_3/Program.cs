bcefbbcf27e7c441aed58fd7913cc1c63ff57fd0
Zaar <zaarinc@gmail.com>
2023-03-14 11:14:15 +0300

    lesson 3 -  3_3

--- Lesson_3/3_3/Program.cs
+++ Lesson_3/3_3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая принимает на вход число (N) 
+//    и выдаёт таблицу квадратов чисел от 1 до N.
+
+void Pow(int num)
+{
+    int i = 1;
+    while (i<=num)
+    {
+        Console.WriteLine(Math.Pow(i,2));
+        i++;
+    }
+
+}
+
+
+int number = int.Parse(Console.ReadLine()!);
+Pow(number);
