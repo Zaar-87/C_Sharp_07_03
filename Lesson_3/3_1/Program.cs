67ff0bb2b2f2af9ffc4f7a452cd26d9077f0246a
Zaar <zaarinc@gmail.com>
2023-03-14 10:53:55 +0300

    lesson 3 -  3_1

--- Lesson_3/3_1/Program.cs
+++ Lesson_3/3_1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая по заданному номеру четверти,
+//    показывает диапазон возможных координат точек в этой четверти (x и y).
+
+GetQuarter();
+
+void GetQuarter()
+{
+    Console.WriteLine("Please, enter quarter ");
+    int q = int.Parse(Console.ReadLine()!);
+    if (q < 1 || q > 4)
+    {
+        Console.WriteLine("Error");
+    }
+    else
+    {
+        switch(q)
+        {
+            case 1:
+                Console. WriteLine("x>0, y>0");
+                break;
+            case 2:
+                Console. WriteLine("x<0, y>0");
+                break;
+            case 3:
+                Console. WriteLine("x<0, y<0");
+                break;
+            case 4:
+                Console. WriteLine("x>0, y<0");
+                break;
+            default:
+                Console. WriteLine("Error");
+                break;
+        }
+    }
+}
