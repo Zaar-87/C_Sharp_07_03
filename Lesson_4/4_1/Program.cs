93e5dff89bbb90c385f3ab879a22ca27e8a8ebc0
Zaar <zaarinc@gmail.com>
2023-03-17 10:31:28 +0300

    lesson 4 -  4_1

--- Lesson_4/4_1/Program.cs
+++ Lesson_4/4_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+
+int num = GetUserNumber();
+int digits = CountNumbers(num);
+Console.WriteLine(digits);
+
+int GetUserNumber()
+{
+    int number = 0;
+    while(number == 0)
+    {
+        Console.Write("Please enter number: ");
+        number = int.Parse(Console.ReadLine()!);
+    }
+    return number;
+}
+
+int CountNumbers(int n)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        n = n/10; 
+        count++;
+    }
+    return count;
+}
