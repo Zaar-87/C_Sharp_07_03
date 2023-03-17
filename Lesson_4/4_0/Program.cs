6c6c9fbe00aef098284d490a1a301c573fd2d7cf
Zaar <zaarinc@gmail.com>
2023-03-17 10:22:03 +0300

    lesson 4 -  4_0

--- Lesson_4/4_0/Program.cs
+++ Lesson_4/4_0/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает
+//
+
+int SumNum(int num)
+{
+    int sum_all = 0;
+
+    for(int i = 1; i <= num; i++)
+        sum_all += i;
+
+    return sum_all;
+}
+
+int val = int.Parse(Console.ReadLine()!);
+int result = SumNum(val);
+Console.WriteLine(result);
