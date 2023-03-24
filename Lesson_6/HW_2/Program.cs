1bf19174d9a620cbfae4608009ff3e47eff8c744
Zaar <zaarinc@gmail.com>
2023-03-24 11:59:12 +0300

    Lesson 6 - classwork

--- Lesson_6/HW_2/Program.cs
+++ Lesson_6/HW_2/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая будет создавать 
+//копию заданного двумерного массива с помощью поэлементного 
+//копирования.
