0ff7f7af86a995c159fcdc984b1873be3b8c7d4b
Zaar <zaarinc@gmail.com>
2023-03-28 20:21:08 +0300

    Lesson 7 - HW_0

--- Lesson_7/HW_0/Program.cs
+++ Lesson_7/HW_0/Program.cs
@@ -4,3 +4,51 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
 */
+
+//input data
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive: ");
+int start = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the max number of massive: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(row_num, column_num, start, stop);
+
+Console.WriteLine("initial array: ");
+Print(mass);
+
+//Functions
+
+//2D array output
+void Print(double[,] array)
+{
+    int row_size = array.GetLength(0);
+    int column_size = array.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {array[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//filling a two-dimensional array with random numbers from a range specified by the user
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] array = new double[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            array[i, j] = Math.Round(n_new.NextDouble() * (to - from) + from, 2);
+
+    return array;
+}
