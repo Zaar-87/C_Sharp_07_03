c2080592a4033c5dd620e4e2d71c45c7c3406311
Zaar <zaarinc@gmail.com>
2023-03-28 19:54:18 +0300

    Lesson 6 - HW added

--- Lesson_6/1_3/Program.cs
+++ Lesson_6/1_3/Program.cs
@@ -4,29 +4,18 @@
 //Если N = 7 -> 0 1 1 2 3 5 8
 
 Console.Write("n=");
-int n=int.Parse(Console.ReadLine()!);
-/*
-void Fibonacci(int n){
-    int a=0;
-    int b=1;
-    Console.Write($"{a} {b} ");
-
-    for (int i=2;i<n;i++){
-        Console.Write($"{a+b} ");
-        (a,b)=(b,a+b);
-
-    }
-}*/
+int n = int.Parse(Console.ReadLine()!);
 
 Fibonacci(n);
 
-void Fibonacci(int n){
-    int a=0;
-    int b=1;
+void Fibonacci(int n)
+{
+    int a = 0;
+    int b = 1;
 
-    for (int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         Console.Write($"{a} ");
-        (a,b)=(b,a+b);
-
+        (a, b) = (b, a + b);
     }
 }
--- Lesson_6/HW_0/Program.cs
+++ Lesson_6/HW_0/Program.cs
@@ -2,3 +2,24 @@
 //Посчитайте, сколько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2
 //-1, -7, 567, 89, 223-> 3
+
+//Functions
+int CountNum()
+{
+    int count = 0;
+    string entered_numbers;
+
+    while (true)
+    {
+        Console.Write("Enter any number: ");
+        entered_numbers = Console.ReadLine()!;
+
+        if (entered_numbers == "") return count;
+        else if (int.Parse(entered_numbers) > 0) count += 1;        
+    }
+}
+
+int count = CountNum();
+
+//result
+Console.WriteLine($"Numbers greater than zero: {count}");
