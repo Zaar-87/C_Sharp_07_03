9671e8491d6c61a2bc481971b5e60d28f2152400
Zaar <zaarinc@gmail.com>
2023-04-04 12:53:34 +0300

    Lesson 8 - HW_4

--- Lesson_8/HW_3/Program.cs
+++ Lesson_8/HW_3/Program.cs
@@ -51,3 +51,57 @@ int[,] MassNums(int row, int column, int from, int to)
 
     return array;
 }
+
+//finding min element
+int[] MinEl(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    int min_num = arr[0, 0];
+    int[] indexes = new int[2];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            if (min_num > arr[i, j])
+            {
+                min_num = arr[i, j];
+                indexes[0] = i;
+                indexes[1] = j;
+            }
+    }
+    Console.Write($"Minimum element on position '{indexes[0]+1},{indexes[1]+1}': ");
+    Console.WriteLine(arr[indexes[0], indexes[1]]);
+    return indexes;
+}
+
+//deleting row and column
+int[,] WithoutRowColumn(int[,] arr, int[] m_indexes)
+{
+    int k = 0;
+    int p = 0;
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    int[,] new_matr = new int[row - 1, column - 1];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            if (m_indexes[0] != i && m_indexes[1] != j)
+            {
+                new_matr[k, p] = arr[i, j];
+                p += 1;
+            }
+        }
+        p = 0;        
+        if (m_indexes[0] != i) k += 1;        
+    }
+    return new_matr;
+}
+
+//result
+int[] new_mass = MinEl(mass);
+int[,] res_mass = WithoutRowColumn(mass, new_mass);
+Console.WriteLine("Result massive:");
+Print(res_mass);
